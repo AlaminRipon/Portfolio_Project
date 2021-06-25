907458942ee15035e7a0ba25540f738f9d84ef4d
alamin <alaminripon5654@gmail.com>
2021-06-26 03:08:35 +0600

    Blog template created

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
+import jobs
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+def home(request):
+  context = {
+    'jobs': Job.objects
+  }
+  return render(request, 'jobs/home.html', context)
