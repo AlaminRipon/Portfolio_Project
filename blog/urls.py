40374118c385b8c7f619e996f5e8b67be7aedd66
alamin <alaminripon5654@gmail.com>
2021-06-26 22:08:39 +0600

    Project Completed

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('blogs/', views.blogs, name='blog'),
+  path('blogs/', views.blogs, name='blogs'),
+  path('blogs/<int:blog_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 def blogs(request):
+  context = {
+    'blogs': Blog.objects
+  }
+  return render(request, 'blogs/blog.html', context)
 
-  return render(request, 'blogs/blog.html')
+def detail(request, blog_id):
+
+  detail_blog =  get_object_or_404(Blog, pk=blog_id)
+
+  return render(request, 'blogs/detail.html', {'blog':detail_blog})
